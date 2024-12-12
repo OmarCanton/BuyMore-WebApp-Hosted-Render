3feac0a8b149a22ed1fb2cfa817b9cfac4b3e6a4
OmarCanton <146691481+OmarCanton@users.noreply.github.com>
2024-12-12 03:51:27 +0000

    Update MenuOptions.jsx

--- Client/src/Components/MenuOptions.jsx
+++ Client/src/Components/MenuOptions.jsx
@@ -8,84 +8,86 @@ export default function MenuOptions() {
     const {openMenu, setOpenMenu} = useContext(userDetailsContext)
     const {theme} = useContext(themesContext)
     return (
-        <div className="menuWrapper">
-            <Dialog 
-                open={openMenu}
-                onClose={(event, reason) => {
-                    if(reason === 'backdropClick' || reason === 'escapeKeyDown') {
-                        setOpenMenu(false)
-                    }
-                }}
-                disableScrollLock
-                // PaperProps={}
-                TransitionComponent={Slide}
-                TransitionProps={{direction: 'right'}}
+        <Dialog 
+            open={openMenu}
+            onClose={(event, reason) => {
+                if(reason === 'backdropClick' || reason === 'escapeKeyDown') {
+                    setOpenMenu(false)
+                }
+            }}
+            disableScrollLock
+            PaperProps={{style: {
+                ...theme === 'dark' && {backgroundColor: '#3C3C3C'},
+                padding: 0,
+                margin: 0,
+                height: '100vh',
+                position: 'absolute',
+                left: '0vh',
+
+
+            }}}
+            TransitionComponent={Slide}
+            TransitionProps={{direction: 'right'}}
+        >
+            <div className="menus">
+            <Link 
+                to='/' 
+                className='home'
             >
-                <div className="menus">
-                <Link 
-                    to='/' 
-                    className='home'
-                >
-                    <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
-                        <HomeRounded 
-                            fontSize='large' 
-                            htmlColor='white'  
-                            style={{...theme == 'light' && window.innerWidth <= 768 && {color: '#3C3C3C'}}}
-                        />
+                <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
+                    <HomeRounded 
+                        fontSize='large' 
+                        htmlColor='white'  
+                    />
+                </Button>
+            </Link>
+            <Link 
+                to='/shop' 
+                className='shop'
+            >
+                <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
+                    <ShopRounded  
+                        fontSize='large' 
+                        htmlColor='white'
+                    />
                     </Button>
-                </Link>
-                <Link 
-                    to='/shop' 
-                    className='shop'
-                >
-                    <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
-                        <ShopRounded 
-                            style={{...theme == 'light' && window.innerWidth <= 768 && {color: '#3C3C3C'}}} 
-                            fontSize='large' 
-                            htmlColor='white'
-                        />
-                        </Button>
-                </Link>
-                <Link 
-                    to='/cart' 
-                    className='wishlist-panel'
-                >
-                    <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
-                        <ShoppingBag 
-                            style={{...theme == 'light' && window.innerWidth <= 768 && {color: '#3C3C3C'}}} 
-                            htmlColor='white' 
-                            fontSize='large' 
-                        />                
-                        {/* <div className="numOfItems">{wishlistItems.length}</div> */}
-                    </Button>
-                </Link>
-                <Link 
-                    to='/favorites' 
-                    className='favorites'
-                >
-                    <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
-                        <FavoriteRounded 
-                            fontSize='large' 
-                            htmlColor='white'
-                            style={{...theme == 'light' && window.innerWidth <= 768 && {color: '#3C3C3C'}}}
-                        />
-                        {/* <div className="numOfItems">{favorites.length}</div> */}
-                    </Button>
-                </Link>
-                <Link 
-                    to='/settings' 
-                    className='settings'
-                >
-                    <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
-                        <SettingsRounded 
-                            fontSize='large' 
-                            htmlColor='white'
-                            style={{...theme == 'light' && window.innerWidth <= 768 && {color: '#3C3C3C'}}}
-                        />
-                    </Button>
-                </Link>
-                </div>
-            </Dialog>
-        </div>
+            </Link>
+            <Link 
+                to='/cart' 
+                className='wishlist-panel'
+            >
+                <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
+                    <ShoppingBag 
+                        htmlColor='white' 
+                        fontSize='large' 
+                    />                
+                    {/* <div className="numOfItems">{wishlistItems.length}</div> */}
+                </Button>
+            </Link>
+            <Link 
+                to='/favorites' 
+                className='favorites'
+            >
+                <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
+                    <FavoriteRounded 
+                        fontSize='large' 
+                        htmlColor='white'
+                    />
+                    {/* <div className="numOfItems">{favorites.length}</div> */}
+                </Button>
+            </Link>
+            <Link 
+                to='/settings' 
+                className='settings'
+            >
+                <Button className='but' fullWidth sx={{borderRadius: '50px'}}>
+                    <SettingsRounded 
+                        fontSize='large' 
+                        htmlColor='white'
+                    />
+                </Button>
+            </Link>
+            </div>
+        </Dialog>
     )
 }
