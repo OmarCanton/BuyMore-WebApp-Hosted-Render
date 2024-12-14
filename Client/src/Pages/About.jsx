dd02ba85afda4a275783a7c9eb4d237def3df371
OmarCanton <146691481+OmarCanton@users.noreply.github.com>
2024-12-14 11:22:34 +0000

    Update About.jsx

--- Client/src/Pages/About.jsx
+++ Client/src/Pages/About.jsx
@@ -28,7 +28,9 @@ export default function About () {
                 animate={{y: '0', opacity: 1}}
                 exit={{
                     y: '20%',
-                    
+                    transition: {
+                        duration: 1.4
+                    }
                 }}
                 transition={{duration: 0.15, ease: 'anticipate'}}
                 className="main"  
@@ -52,7 +54,7 @@ export default function About () {
                     animate={{y: 0, opacity: 1}}
                     exit={{
                         y: '20%', 
-                        opacity: 0,
+                        opacity: 0
                     }}
                     transition={{duration: 0.1, delay: 0.14, ease: 'anticipate'}}
                 >
