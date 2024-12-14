f457b6d6aeacc097d33e2ffa6021db135290170e
OmarCanton <146691481+OmarCanton@users.noreply.github.com>
2024-12-14 16:53:22 +0000

    update

--- Client/src/Pages/PageNotFound.jsx
+++ Client/src/Pages/PageNotFound.jsx
@@ -1,5 +1,6 @@
 import PageNotFoundAnime from '../Effects/PageNotFoundAnime.json'
 import Lottie from 'lottie-react'
+import '../Styles/PageNotFound.css'
 
 export default function PageNotFound () {
     return(
