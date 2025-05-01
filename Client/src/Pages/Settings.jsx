8a5b836897701ed7ae551cf446ed873e3ff3b931
OmarCanton <abdulaiumar20@gmail.com>
2025-05-01 21:15:34 +0000

    Update Settings.jsx

--- Client/src/Pages/Settings.jsx
+++ Client/src/Pages/Settings.jsx
@@ -252,7 +252,7 @@ export default function Settings () {
                 }
             </div>
             <div className="footer">
-                <p>Version: 1.0.0</p>
+                <p>Version: 1.2.0</p>
             </div>
             <Panel />
         </div>
